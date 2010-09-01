5217a36437d69eb1a40b649b8f27507f76aa3753
Daniel Ruoso <daniel@ruoso.com>
2010-09-01 11:52:03 -0300

    sketching what the main Zumbis app would look like

---
+++ examples/zumbis/lib/Zumbis.pm
@@ -0,0 +1,51 @@
+package Zumbis;
+use base 'Jogo';
+use strict;
+use warnings;
+
+use constant state_map =>
+  { start =>
+    { controller => 'Intro',
+      transitions =>
+      { continue => 'menu',
+        abort => 'end'
+      },
+    },
+    menu =>
+    { controller => 'Menu',
+      transitions =>
+      { start_male => sub {
+            my $self = shift;
+            $self->transition('ingame', gender => 'male');
+        },
+        start_female => sub {
+            my $self = shift;
+            $self->transition('ingame', gender => 'female');
+        },
+        quit => 'credits'
+      },
+    },
+    ingame =>
+    { controller => 'InGame',
+      transitions =>
+      { died => sub {
+            my $self = shift;
+            my $time = $self->active->time_survived;
+            my $score =  $self->active->score;
+            $self->transition('gameover',
+                              time => $time,
+                              score => $score);
+        },
+        quit => 'menu',
+      },
+    },
+    gameover =>
+    { controller => 'GameOver',
+      transitions =>
+      { finish => 'end'
+      },
+    },
+  };
+
+
+1;
